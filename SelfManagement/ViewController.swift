a2da61f3bf1ba1f4225dc40ed9a1396fc9d4297b
hosakaren <hosakaren@icloud.com>
2021-11-07 18:48:09 +0900

    Initial Commit

---
+++ SelfManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfManagement
+//
+//  Created by hosakaren on 2021/11/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
